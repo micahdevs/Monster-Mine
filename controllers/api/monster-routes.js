889b9d1b0cd3107b26e8efac2223815a8e0f0f1b
Fisherman <aflee@wisc.edu>
2023-12-13 12:49:31 -0600

    adding server fundamentals

---
+++ controllers/api/monster-routes.js
@@ -0,0 +1,9 @@
+// TO DO. This route summons a monster when clicked on the home page
+
+router.put('/:id', async (req, res ) => {
+    try {} catch {}
+});
+// TO DO This Route Lets a User DELETE a Monster
+router.delete('/:id', async (req, res ) => {
+    try {} catch {}
+});
