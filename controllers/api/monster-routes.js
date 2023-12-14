84bf13c1dc2d71f7d05d5ec30b4f75d784536aa6
Alan Lee <aflee@wisc.edu>
2023-12-13 19:21:47 -0600

    adding touches on router and structure of seeds

--- controllers/api/monster-routes.js
+++ controllers/api/monster-routes.js
@@ -1,4 +1,25 @@
 // TO DO. This route summons a monster when clicked on the home page
+const router = require('express').Router();
+
+//TO DO - Update the Model
+const { Monster } = require('../models/index.js');
+
+router.get('/:id', async (req, res ) => {
+    try {
+      const dbMonsterPostData = await Monster.findbyPk({
+          where: {id:req.params.id}, //TO DO Update the property tag in the where to be the User ID
+        });
+      const monster_posts = dbMonsterPostData.map((posts) => {
+        posts.get({ plain: true })
+      });
+      res.render('monster-detail-sheet', { //TO CHECK - Make sure the handlebar tag matches
+        monster_posts,
+      });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+});
 
 router.put('/:id', async (req, res ) => {
     try {} catch {}
