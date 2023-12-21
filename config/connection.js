095a2f74d68bf23aa551602f8e2a2946fa7c2116
Micah Ives <micbenives@gmail.com>
2023-12-20 20:35:35 -0600

    Update Sequelize migration command

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,14 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    }
-  );
-  
-  module.exports = sequelize;
+const sequelize = new Sequelize(process.env.JAWSDB_URL, {
+  logging: true, // Logs SQL queries
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: true, // Important if you are using a secure connection
+    },
+  },
+});
+
+module.exports = sequelize;
